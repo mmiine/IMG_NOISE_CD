ce2f4e172b82e1a5b3d4eba7a17a12859314f5be
mmiine <mminegundogan@gmail.com>
2020-09-07 23:16:34 +0300

    correction

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import sys
 import argparse
 import cv2 as cv
 from denoising.addnoise import (gaussian, salt_pepper)
-from denoising.denoise import (bm3d, band_reject)
+from denoising.denoise import (bm3d, wavelet)
 
 
 parser = argparse.ArgumentParser('Test an image')
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     elif mode == 'bm3d':
         dumb = bm3d(im)
     elif mode == 'wavelet':
-        dumb = band_reject(im,200,600)
+        dumb = wavelet(im)
 
     cv.imshow(str(mode),dumb)
     cv.waitKey(0)
